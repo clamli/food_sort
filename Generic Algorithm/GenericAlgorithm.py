7900c3662a59436cbe85aaea48116d1e06247321
clamli <boyizjuer2017@gmail.com>
2017-10-23 23:35:44 -0700

    add plot function and add different dataset

--- Generic Algorithm/GenericAlgorithm.py
+++ Generic Algorithm/GenericAlgorithm.py
@@ -2,28 +2,25 @@ import matplotlib.pyplot as plt
 import myutil as mtl
 import math
 import numpy as np
+from copy import deepcopy
 
 DNA_SIZE = 10  # DNA size
 CROSS_RATE = 0.2
 MUTATE_RATE = 0.01
 POP_SIZE = 500
-N_GENERATIONS = 1000
+N_GENERATIONS = 100
 
 
 class GA(object):
-	def __init__(self, DNA_size, cross_rate, mutation_rate, pop_size, value_bound=10, pop='default', theta1=10.0, theta2=1.0, theta3=1.0, theta4=3.0):
+	def __init__(self, DNA_size, cross_rate, mutation_rate, pop_size, value_bound=10, pop_type='default', theta1=10.0, theta2=1.0, theta3=1.0, theta4=3.0):
 		self.DNA_size = DNA_size          # food sequence size
 		self.cross_rate = cross_rate
 		self.mutate_rate = mutation_rate
 		self.pop_size = pop_size          # sequence pool size
 		self.value_bound = value_bound    # health/taste bound
 		#### define how to generate data
-		if pop == 'default':
-			target = [np.random.randint(0, self.value_bound, size=2) for _ in range(self.DNA_size)]
-			self.pop = np.vstack(np.random.permutation(target) for _ in range(self.pop_size)).reshape(self.pop_size, self.DNA_size, 2)
-		else:
-			target = self.generate_init_data(pop)
-			self.pop = np.vstack(np.random.permutation(target) for _ in range(self.pop_size)).reshape(self.pop_size, self.DNA_size, 2)
+		self.target = [np.random.randint(0, self.value_bound, size=2) for _ in range(self.DNA_size)]	
+		self.generate_init_data(pop_type, self.target)
 		#### set necessary parameters for generic algorithm
 		self.left_bound = int(DNA_size/2)
 		self.right_bound = self.left_bound
@@ -32,16 +29,29 @@ class GA(object):
 		self.theta3 = theta3
 		self.theta4 = theta4
 
-	def generate_init_data(self, func_type):
+	def change_parameters(self, theta1, theta2, theta3, theta4):
+		self.theta1 = theta1
+		self.theta2 = theta2
+		self.theta3 = theta3
+		self.theta4 = theta4
+
+	def generate_init_data(self, pop_type, init_items):
 		'''Generate different kinds of init data'''
+		if pop_type == 'default':
+			self.pop = np.vstack(np.random.permutation(init_items) for _ in range(self.pop_size)).reshape(self.pop_size, self.DNA_size, 2)
+			return
 		ret_result = []
-		if func_type == 'inverse proportional function':
-			for x in range(1, self.DNA_size+1):
-				ret_result.append([x, 20/x])
-		elif func_type == 'directly proportional function':
-			for x in range(1, self.DNA_size+1):
-				ret_result.append([x, x])
-		return ret_result
+		taste_lst = [init_items[i][0] for i in range(len(init_items))]
+		health_lst = [init_items[i][1] for i in range(len(init_items))]
+		if pop_type == 'inverse proportional function':
+			taste_lst.sort()
+			health_lst.sort(reverse=True)
+		elif pop_type == 'directly proportional function':
+			taste_lst.sort()
+			health_lst.sort()
+		for t, h in zip(taste_lst, health_lst):
+			ret_result.append([t, h])
+		self.pop = np.vstack(np.random.permutation(ret_result) for _ in range(self.pop_size)).reshape(self.pop_size, self.DNA_size, 2)
 
 	def get_fitness(self):
 		fitness = np.empty((self.pop_size,), dtype=np.float64)
@@ -118,27 +128,99 @@ class GA(object):
 			parent[:] = child
 		self.pop = pop
 
-ga = GA(DNA_size=DNA_SIZE, cross_rate=CROSS_RATE, mutation_rate=MUTATE_RATE, pop_size=POP_SIZE, value_bound=10, pop='directly proportional function', theta1=1.0, theta2=0.0)
-# print("first---------", ga.pop)
-# env = TravelSalesPerson(N_CITIES)
-best_fit_lst = []
+
+ga = GA(DNA_size=DNA_SIZE, cross_rate=CROSS_RATE, mutation_rate=MUTATE_RATE, pop_size=POP_SIZE, value_bound=10, pop_type='default', theta1=1.0, theta2=0.0)
+best_fit_lst1 = []
+best_fit_lst2 = []
+best_fit_lst3 = []
+best_seq1 = []
+best_seq2 = []
+best_seq3 = []
+max_seq1 = []
+max_seq2 = []
+max_se13 = []
 plt.figure(1)
+plt.figure(2)
+plt.figure(3)
+plt.figure(4)
+ax1 = plt.subplot(311)
+ax2 = plt.subplot(312)
+ax3 = plt.subplot(313)
+
+#### for default target ####
+max_fitness = 0
 for generation in range(N_GENERATIONS):
-	# lx, ly = ga.translateDNA(ga.pop, env.city_position)
 	fitness = ga.get_fitness()
 	best_idx = np.argmax(fitness)
 	print('Gen:', generation, '| best fit: %.2f' % fitness[best_idx])
 	print('Best Sort:\n', ga.pop[best_idx])
-	best_fit_lst.append(fitness[best_idx])
-	# print("result:", ga.pop[best_idx][0:ga.left_bound].tolist())
-	# print(ga.left_bound)
-	ga.evolve(fitness)   
+	best_fit_lst1.append(fitness[best_idx])
+	if fitness[best_idx] >= max_fitness:
+		max_seq1 = deepcopy(ga.pop[best_idx])
+	ga.evolve(fitness)
 
-    # env.plotting(lx[best_idx], ly[best_idx], total_distance[best_idx])
+#### for inverse proportional function target ####
+max_fitness = 0
+ga.generate_init_data('inverse proportional function', ga.target)
+for generation in range(N_GENERATIONS):
+	fitness = ga.get_fitness()
+	best_idx = np.argmax(fitness)
+	print('Gen:', generation, '| best fit: %.2f' % fitness[best_idx])
+	print('Best Sort:\n', ga.pop[best_idx])
+	best_fit_lst2.append(fitness[best_idx])
+	if fitness[best_idx] >= max_fitness:
+		max_seq2 = deepcopy(ga.pop[best_idx])
+	ga.evolve(fitness) 
 
-# plt.ioff()
-plt.title("Change of Best Fitness")
+#### for directly proportional function target ####	
+max_fitness = 0
+ga.generate_init_data('directly proportional function', ga.target)
+for generation in range(N_GENERATIONS):
+	fitness = ga.get_fitness()
+	best_idx = np.argmax(fitness)
+	print('Gen:', generation, '| best fit: %.2f' % fitness[best_idx])
+	print('Best Sort:\n', ga.pop[best_idx])
+	best_fit_lst3.append(fitness[best_idx])
+	if fitness[best_idx] >= max_fitness:
+		max_seq3 = deepcopy(ga.pop[best_idx])
+	ga.evolve(fitness)  
+
+#### plot result ####
+plt.figure(1)
+plt.title("Best taste and health trend for each dataset")
+plt.xlabel("item number")
+plt.ylabel("taste/health value")
+plt.plot(range(1, len(max_seq1)+1), [max_seq1[i][0] for i in range(len(max_seq1))], 'r-', label="taste_d", marker='*')
+plt.plot(range(1, len(max_seq1)+1), [max_seq1[i][1] for i in range(len(max_seq1))], 'r--', label="health_d", marker='*')
+plt.xlim(0.0, 10.0)
+plt.ylim(0.0, 10.0)
+plt.figure(2)
+plt.title("Best taste and health trend for each dataset")
+plt.xlabel("item number")
+plt.ylabel("taste/health value")
+plt.plot(range(1, len(max_seq2)+1), [max_seq2[i][0] for i in range(len(max_seq2))], 'g-', label="taste_iv", marker='*')
+plt.plot(range(1, len(max_seq2)+1), [max_seq2[i][1] for i in range(len(max_seq2))], 'g--', label="health_iv", marker='*')
+plt.xlim(0.0, 10.0)
+plt.ylim(0.0, 10.0)
+plt.figure(3)
+plt.title("Best taste and health trend for each dataset")
+plt.xlabel("item number")
+plt.ylabel("taste/health value")
+plt.plot(range(1, len(max_seq3)+1), [max_seq3[i][0] for i in range(len(max_seq3))], 'b-', label="taste_dp", marker='*')
+plt.plot(range(1, len(max_seq3)+1), [max_seq3[i][1] for i in range(len(max_seq3))], 'b--', label="health_dp", marker='*')
+plt.xlim(0.0, 10.0)
+plt.ylim(0.0, 10.0)
+
+#### plot result ####
+plt.figure(4)
+plt.title("Change of Best Fitness for different dataset")
 plt.xlabel("Generation")
 plt.ylabel("Best Fitness")
-plt.plot(best_fit_lst)
+plt.sca(ax1)
+plt.plot(best_fit_lst1)
+plt.sca(ax2)
+plt.plot(best_fit_lst2)
+plt.sca(ax3)
+plt.plot(best_fit_lst3)
+
 plt.show()
